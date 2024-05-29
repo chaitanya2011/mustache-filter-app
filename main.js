a7b81ec7b3fdd40f654ddd79bbe8ddea7e9ec286
chaitanya2011 <nishagurkhude@gmail.com>
2024-05-29 13:40:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
+noseX=0;
+noseY=0;
+
 function preload() {
+    clown_nose=loadImage('https://i.postimg.cc/FsnhhYNF/download-1.jpg');
 }
 
 function setup() {
 canvas = createCanvas (300, 300); 
 canvas.center();
+video=createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+
+poseNet=ml5.poseNet(video,modlLoded);
+poseNet.on('pose',gotPoses);
+}
+
+function modlLoded(){
+    console.log('PosNet Is Initialized');
 }
 
 function draw() {
+    image(video,0,0,300,300);
+    image(clown_nose,noseX,noseY,30,30);
 }
 
 function take_snapshot(){
-save('myFilterImage.png');
+save('download(1).jpg');
+}
+
+function gotPoses(results)
+{
+  if(results.length>0)
+    {
+       console.log(results);
+       noseX=results[0].pose.nose.x-10;
+       noseY=results[0].pose.nose.y;
+       console.log("nose x="+noseX);
+       console.log("nose y="+noseY);
+    }
 }
